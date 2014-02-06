57fcb1a5c1976ecb283772dd8440122d07d11a3d
Haydn Strauss <strauss.haydn@gmail.com>
2014-02-06 15:50:18 -0800

    Updated for Firefox and applied styling

--- js/script.js
+++ js/script.js
@@ -6,19 +6,41 @@ function addUser(username, email){
     query.find({
         success: function(results) {
             if (results.length > 0){
-                console.log('user exists');
+                $("#error").show();
             }
             else{
                 var userObject = new UserObject();
                 userObject.save({username: username, email: email}, {
                     success: function(object) {
-                        console.log('added user')
-                        $(".success").show();
+                        $("#reserve_username_form").hide();
+                        $(".success").fadeIn();
                     },
                     error: function(model, error) {
-                        $(".error").show();
+                        $("#error").show();
                     }
                 });
+            }
+        },
+        error: function(error) {
+            console.log('error')
+
+        }
+    });
+}
+
+
+function userExists(username){
+    var UserObject = Parse.Object.extend("PreRegUsers");
+
+    var query = new Parse.Query(UserObject);
+    query.equalTo("username", username);
+    query.find({
+        success: function(results) {
+            if (results.length > 0){
+                return "test"
+            }
+            else{
+                return "test1"
             }
         },
         error: function(error) {
